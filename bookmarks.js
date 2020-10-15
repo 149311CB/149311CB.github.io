3bff297bc0be3a15fcb76de0e2f2fa25ea085ff0
Nick <an.nickhcm2000@gmail.com>
2020-10-15 20:21:19 +0700

    homepage

---
+++ bookmarks.js
@@ -0,0 +1,27 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Work",
+    links: [
+      { name: " /Mail", url: "https://inbox.google.com" },
+      { name: " /Git", url: "https://github.com" },
+      { name: " /Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Work Again",
+    links: [
+      { name: " /Youtube", url: "https://youtube.com" },
+      { name: " /Pluralsight", url: "https://pluralsight.com" },
+      { name: " /Skillshare", url: "https://skillshare.com" },
+    ],
+  },
+  {
+    title: "I said work! God damn it!",
+    links: [
+      { name: " /Notion", url: "https://Notion.so" },
+      { name: " /Wps", url: "https://wps.com" },
+      { name: " /Portal", url: "/https://portal.huflit.edu.vn" },
+    ],
+  },
+];
